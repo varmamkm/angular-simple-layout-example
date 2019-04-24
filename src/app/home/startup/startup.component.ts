8b5b1cbdb3ed79983179080ca6d54cae2a454ea3
varmamkm <varmamkm@gmail.com>
2019-04-24 15:06:03 +0200

    1 file modified

--- src/app/home/startup/startup.component.ts
+++ src/app/home/startup/startup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AparmentselectionService } from '../aparmentselection.service';
 import { Subscription }   from 'rxjs';
+import { ActivatedRoute,Router  } from '@angular/router';
 
 @Component({
   selector: 'app-startup',
@@ -13,12 +14,17 @@ subscription: Subscription;
 selectedapartmentid:number = 0;
 newvalue:string="4";
 
-  constructor(private apartmentsectionService: AparmentselectionService) {
+  constructor(private apartmentsectionService: AparmentselectionService,
+                   private route: ActivatedRoute,
+                   private rte:Router) {
     this.subscription = apartmentsectionService.selectionChanged$.subscribe(
       selectedapartmentid => { 
         this.selectedapartmentid = selectedapartmentid;
         console.log("the selection in header changed to :",this.selectedapartmentid)
         });
+
+        console.log("URL2",this.route.snapshot.url);
+        console.log("URL1",this.rte.url);
    }
 
    addtoheader()
